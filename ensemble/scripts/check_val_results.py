b217a2d8e7a453bdbb0835c3ab0e232a40bcc661
Marcel Roth <MarcelRoth100@aol.com>
2024-08-30 23:51:45 +0200

    add sweep

--- ensemble/scripts/check_val_results.py
+++ ensemble/scripts/check_val_results.py
@@ -1,16 +1,19 @@
 import argparse
+import logging
 import os
 import re
 from collections import namedtuple, Counter
 
-CheckpointMetadata = namedtuple('CheckpointMetadata', ['rel_path', 'base_name', 'wandb_name', 'sweep_id'])
+import wandb
+
+CheckpointMetadata = namedtuple('CheckpointMetadata', ['rel_path', 'base_name', 'wandb_name', 'sweep_id', 'run_id'])
 
 
-def find_ckpt_files(dir):
+def find_files_with_ending(dir, ending='.ckpt'):
     ckpt_files = []
     for root, dirs, files in os.walk(dir):
         for file in files:
-            if file.endswith('.ckpt'):
+            if file.endswith(ending):
                 ckpt_files.append(os.path.join(root, file))
     return ckpt_files
 
@@ -29,7 +32,7 @@ def get_base_name(path):
 
         basename = os.path.basename(path)
         wandb_name = basename.split('_')[0]
-        return CheckpointMetadata(path, basename, wandb_name, sweep_id)
+        return CheckpointMetadata(path, basename, wandb_name, sweep_id, None)
 
     if path.startswith('checkpoints/run'):
         pattern = r"run-\d{8}_\d{6}-(\w+-\w+-\d+)"
@@ -40,26 +43,70 @@ def get_base_name(path):
             print("No wandb name found for run.")
             raise ValueError("Es knallt!")
         basename = os.path.basename(path)
-        return CheckpointMetadata(path, basename, wandb_name, None)
+        return CheckpointMetadata(path, basename, wandb_name, None, None)
     else:
         raise ValueError("Es knallt gewaltig!")
 
 
-def check_val_results(checkpoint_dir):
-    ckpt_dirs = find_ckpt_files(checkpoint_dir)
-    ckpt_infos = [get_base_name(ckpt) for ckpt in ckpt_dirs]
-    print("\n".join(map(str, ckpt_infos)))
-
+def check_duplicate_checkpoints(ckpt_infos):
     x = set()
+    counts = 0
     for wandb_name, sweep_id in list(map(lambda _x: (_x.wandb_name, _x.sweep_id), ckpt_infos)):
         if (wandb_name, sweep_id) in x:
             print(f"Duplicate found: {wandb_name}, {sweep_id}")
+            counts += 1
         x.add((wandb_name, sweep_id))
+    if counts > 0:
+        return False
+    return True
+
+
+def get_finished_wandb_runs():
+    api = wandb.Api()
+    runs = api.run(f'wuesuv/CV2024')
+
+    finished_wandb_runs = []
+    for run in runs:
+        if run.state != 'running':
+            sweep_id = run.sweep.id if run.sweep else None
+            finished_wandb_runs = (run.name, run.id, sweep_id)
+        else:
+            logging.info(f"Excluded running run: {run.name} ({run.id})")
+    return finished_wandb_runs
+
+
+def check_val_results(checkpoint_dir, result_dir):
+    ckpt_dirs = find_files_with_ending(checkpoint_dir, ending='.ckpt')
+    ckpt_infos = [get_base_name(ckpt) for ckpt in ckpt_dirs]
+
+    assert check_duplicate_checkpoints(ckpt_infos)
+
+    finished_wandb_runs = get_finished_wandb_runs()
+
+    result = []
+    for ckpt_info in ckpt_infos:
+        for wandb_name, run_id, sweep_id in finished_wandb_runs:
+            if wandb_name in ckpt_info and sweep_id in ckpt_info:
+                result.append(ckpt_info._replace(run_id=run_id))
+            else:
+                print(f"Checkpoint {ckpt_info} not found in finished wandb runs.")
+    print(len(result))
+
+
+
+    #
+    # val_metric_filenames = find_files_with_ending(result_dir, ending='.json')
+    # # peaky-firefly-5_5afkssf.json
+    # val_pred_filenames = find_files_with_ending(result_dir, ending='.csv')
+    # # peaky-firefly-5_5afkssf.csv
+    #
 
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser(description='Check validation results.')
     argparse.add_argument('--checkpoint_dir', default="checkpoints", type=str,
                           help='Path to the checkpoint directory.')
+    argparse.add_argument('--result_dir', default="ensemble/val_results", type=str,
+                          help='Path to the model validation results.')
     args = argparse.parse_args()
 
     check_val_results(args.checkpoint_dir)
