2280b3f9ea09659d555d8220adf1ac5d827459f4
Marcel Roth <MarcelRoth100@aol.com>
2024-08-31 00:14:15 +0200

    add sweep

--- ensemble/scripts/check_val_results.py
+++ ensemble/scripts/check_val_results.py
@@ -64,7 +64,7 @@ def get_finished_wandb_runs():
     api = wandb.Api()
     runs = api.runs(f'wuesuv/CV2024')
 
-    finished_wandb_runs = []
+    finished_wandb_runs, running_wandb_runs = [], []
     for run in runs:
         if run.state != 'running':
             sweep_id = run.sweep.id if run.sweep else None
@@ -78,8 +78,7 @@ def check_val_results(checkpoint_dir, result_dir):
     ckpt_dirs = find_files_with_ending(checkpoint_dir, ending='.ckpt')
     ckpt_infos = [get_base_name(ckpt) for ckpt in ckpt_dirs]
 
-    assert check_duplicate_checkpoints(ckpt_infos)
-
+    # run_name, run_id, sweep_id
     finished_wandb_runs = get_finished_wandb_runs()
 
     result = []
@@ -87,6 +86,8 @@ def check_val_results(checkpoint_dir, result_dir):
         for run_name, run_id, sweep_id in finished_wandb_runs:
             if run_name in ckpt_info and sweep_id in ckpt_info:
                 result.append(ckpt_info._replace(run_id=run_id))
+
+    assert check_duplicate_checkpoints(ckpt_infos)
 
     print(len(result))
 
